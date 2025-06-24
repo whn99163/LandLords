96ea44df2ed3c9847994da2d8deb70e3c51b0480
wanghaonan <wanghaonan_aiit@outlook.com>
2025-06-24 17:57:23 +0800

    玩家类初始化添加

--- player.cpp
+++ player.cpp
@@ -4,3 +4,63 @@ Player::Player(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Player::setName(QString name)
+{
+    m_name=name;
+}
+
+QString Player::getName()
+{
+    return m_name;
+}
+
+void Player::setRole(Player::Role role)
+{
+    m_role = role;
+}
+
+Player::Role Player::getRole()
+{
+    return m_role;
+}
+
+void Player::setSex(Player::Sex sex)
+{
+    m_sex=sex;
+}
+
+Player::Sex Player::getSex()
+{
+    return m_sex;
+}
+
+void Player::setDirection(Player::Direction direction)
+{
+    m_direction = direction;
+}
+
+void Player::getDirection()
+{
+    return m_direction;
+}
+
+void Player::setType(Player::Type type)
+{
+    m_type = type;
+}
+
+Player::Type Player::getType()
+{
+    return m_type;
+}
+
+void Player::setScore(int score)
+{
+    m_score = score;
+}
+
+int Player::getScore()
+{
+    return m_score;
+}
--- player.h
+++ player.h
@@ -7,11 +7,42 @@ class Player : public QObject
 {
     Q_OBJECT
 public:
+    enum Role{Lord,Farmer};
+    enum Sex{Man,Woman};
+    enum Direction{Left,Right};//头像的显示方位
+    enum Type{Robot,User,UnKnow}; //玩家的类型
     explicit Player(QObject *parent = nullptr);
+    explicit Player(QString name,QObject *parent = nullptr);
 
+    void setName(QString name);
+    QString getName();
+
+    void setRole(Role role);
+    Role getRole();
+
+    void setSex(Sex sex);
+    Sex getSex();
+
+    void setDirection(Direction direction);
+    void getDirection();
+
+    void setType(Type type);
+    Type getType();
+
+    void setScore(int score);
+    int getScore();
 signals:
 
 public slots:
+
+private:
+    int m_score;
+    QString m_name;
+    Role m_role;
+    Sex m_sex;
+    Direction m_direction;
+    Type m_type;
+    bool m_isWin;
 };
 
 #endif // PLAYER_H
